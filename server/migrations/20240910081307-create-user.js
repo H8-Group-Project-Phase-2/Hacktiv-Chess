939d324782a78d8a9e2e43297d61373e0f4c2b84
Zhafir <zahfir1000@hotmail.com>
2024-09-10 15:22:05 +0700

    Feat: Login Backend

---
+++ server/migrations/20240910081307-create-user.js
@@ -0,0 +1,46 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      matches: {
+        type: Sequelize.INTEGER,
+      },
+      wins: {
+        type: Sequelize.INTEGER,
+      },
+      draws: {
+        type: Sequelize.INTEGER,
+      },
+      loses: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
