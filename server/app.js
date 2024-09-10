7b0589b448d81b9875b60e65259be0a77727b30c
Zhafir <zahfir1000@hotmail.com>
2024-09-10 15:59:19 +0700

    Feat: Register

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(port, () => {
   console.log("Berlari di port", port);
