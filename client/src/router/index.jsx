ac5578a1b2144b6f152968ab8eb454aaeb0870c9
unknown <dickytaruna67@gmail.com>
2024-09-10 17:43:51 +0700

    register frontend

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "../views/Register";
+import Login from "../views/Login";
 
 const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
