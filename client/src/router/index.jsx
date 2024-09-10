99bbe78d0d283dcad964ee9c50f1d4d8f606c1b2
Gerald Simanullang <geraldsimanullang@gmail.com>
2024-09-10 22:24:58 +0700

    chessboard done

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "../views/Register";
+import Play from "../views/Play";
 
 const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
   },
+  {
+    path: "/play",
+    element: <Play />
+  }
 ]);
 
 export default router;
