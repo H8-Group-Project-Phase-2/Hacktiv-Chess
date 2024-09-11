989a4c1269a8740a9dad2f30dfb6cf59867818dc
unknown <dickytaruna67@gmail.com>
2024-09-11 12:14:13 +0700

    create room form

---
+++ client/src/components/CreateForm.jsx
@@ -0,0 +1,18 @@
+export default function CreateForm() {
+  return (
+    <form className="max-w-xl mx-auto p-4 shadow-lg rounded-lg bg-white">
+      <h2 className="text-2xl font-bold mb-4">Create room</h2>
+
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2">password</label>
+        <input type="password" className="w-full px-3 py-2 border rounded-lg" />
+      </div>
+      <button
+        type="submit"
+        className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700"
+      >
+        Create room
+      </button>
+    </form>
+  );
+}
--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <div>hallo</div>;
+  return (
+    <div class="flex justify-start">
+      <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+        Create room
+      </button>
+    </div>
+  );
 }
