71bb6f000ba66d93ddc2cd208e471c438193acbc
unknown <dickytaruna67@gmail.com>
2024-09-12 09:47:39 +0700

    button refresh

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -33,7 +33,7 @@ export default function HomePage({ url }) {
   };
 
   return (
-    <div className="flex flex-col justify-start">
+    <div classnam="flex flex-col justify-start">
       <div className="flex justify-between">
         <button
           onClick={handleCreateRoom}
@@ -43,7 +43,7 @@ export default function HomePage({ url }) {
         </button>
         <button
           onClick={fetchRooms}
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 flex items-center"
+          className="bg-opacity-100 text-white px-4 py-2 rounded flex items-center"
         >
           <svg
             className="animate-spin h-5 w-5 text-white mr-2"
@@ -59,7 +59,7 @@ export default function HomePage({ url }) {
               d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
             />
           </svg>
-          Refresh Room
+          Refresh
         </button>
       </div>
       <div className="flex justify-end"></div>
